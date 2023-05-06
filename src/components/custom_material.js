541f0ba68b0321cf431cd79040fe9ef1ee431097
Motifff <57866475+Motifff@users.noreply.github.com>
2023-05-06 21:19:53 +0800

    regular update

--- src/components/custom_material.js
+++ src/components/custom_material.js
@@ -2,40 +2,30 @@ import * as THREE from 'three'
 import { extend } from '@react-three/fiber'
 import { shaderMaterial } from '@react-three/drei'
 import glsl from 'babel-plugin-glsl/macro'
+import vShader from '../assets/shaders/vs.glsl'
+import fShader from '../assets/shaders/fg.glsl'
 
-// This shader is from Bruno Simons Threejs-Journey: https://threejs-journey.xyz
 const CustomMaterial = shaderMaterial(
   {
     time: 0,
-    colorStart: new THREE.Color('#505050'),
-    colorEnd: new THREE.Color('black')
   },
   glsl`
       varying vec2 vUv;
       void main() {
-        vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-        vec4 viewPosition = viewMatrix * modelPosition;
-        vec4 projectionPosition = projectionMatrix * viewPosition;
-        gl_Position = projectionPosition;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         vUv = uv;
       }`,
   glsl`
       #pragma glslify: cnoise3 = require(glsl-noise/classic/3d.glsl) 
       uniform float time;
-      uniform vec3 colorStart;
-      uniform vec3 colorEnd;
+      uniform bumpScale
+
       varying vec2 vUv;
       void main() {
-        vec2 displacedUv = vUv + cnoise3(vec3(vUv * 1.0, time * 0.05));
-        float strength = cnoise3(vec3(displacedUv * 10.0, time * 0.2));
-        float outerGlow = distance(vUv, vec2(0.5)) * 2.0 - 0.5;
-        strength += outerGlow;
-        strength += step(-0.2, strength) * 0.6;
-        strength = clamp(strength, 0.0, 1.0);
-        vec3 color = mix(colorStart, colorEnd, strength);
+        float noise = cnoise3(vec3(vUv * 50.0, time * 0.2));
+        vec3 color = vec3(1.0, 0.5 + noise * 0.5, 0.0);
+        
         gl_FragColor = vec4(color, 1.0);
-        #include <tonemapping_fragment>
-        #include <encodings_fragment>
       }`
 )
 
